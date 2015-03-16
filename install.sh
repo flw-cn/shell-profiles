6d2c73424e7eac68288726ad946ea1de9db4580e
flw <flw@cpan.org>
2015-03-16 21:30:42 +0800

    修改文件编码。统一为 utf-8

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 # vim: set fdm=marker:
 
-# 安装 bash 启动脚本 {{{
+# 瀹夎 bash 鍚姩鑴氭湰 {{{
 ln -s ~/shell_profiles/dot_bash_profile_flw ~/.bash_profile_flw
 
 sed -i '/# flw bash script begin/,/# flw bash script end/d' ~/.bash_profile
@@ -14,7 +14,7 @@ fi
 EOD
 # }}}
 
-# 安装 vim 配置文件 {{{
+# 瀹夎 vim 閰嶇疆鏂囦欢 {{{
 ln -s ~/shell_profiles/dot_vimrc_flw ~/.vimrc_flw
 mkdir -p ~/.vim/colors && cp -f flwcolor.vim ~/.vim/colors/
 rm -f ~/.vimrc
@@ -23,27 +23,27 @@ sed -i '/" flw vim script begin/,/" flw vim script end/d' ~/.vimrc && sed -i '1d
 sed -i '1i" flw vim script begin\nif filereadable( expand( "$HOME/.vimrc_flw" ) )\n    source ~/.vimrc_flw\nendif\n" flw vim script end\n' ~/.vimrc
 # }}}
 
-# 安装 screen 配置文件 {{{
+# 瀹夎 screen 閰嶇疆鏂囦欢 {{{
 rm -f ~/.screenrc
 ln -s ~/shell_profiles/dot_screenrc ~/.screenrc
 # }}}
 
-# 安装 readline 配置文件 {{{
+# 瀹夎 readline 閰嶇疆鏂囦欢 {{{
 rm -f ~/.inputrc
 ln -s ~/shell_profiles/dot_inputrc ~/.inputrc
 # }}}
 
-# 安装 gdb 启动脚本 {{{
+# 瀹夎 gdb 鍚姩鑴氭湰 {{{
 rm -f ~/.gdbinit
 ln -s ~/shell_profiles/dot_gdbinit ~/.gdbinit
 # }}}
 
-# 安装 guile 启动脚本 {{{
+# 瀹夎 guile 鍚姩鑴氭湰 {{{
 rm -f ~/.guile
 ln -s ~/shell_profiles/dot_guile ~/.guile
 # }}}
 
-# 安装 sbclrc 启动脚本 {{{
+# 瀹夎 sbclrc 鍚姩鑴氭湰 {{{
 rm -f ~/.sbclrc
 ln -s ~/shell_profiles/dot_sbclrc ~/.sbclrc
 # }}}
