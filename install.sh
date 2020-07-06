ea5717acecc2278d617e7bb29d360ff6d1d537b1
flw <flw@cpan.org>
2020-07-06 18:44:19 +0800

    Refactor: 重构 install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,45 @@
 #!/usr/bin/env bash
 # vim: set fdm=marker:
 
-# 安装 bash 启动脚本 {{{
-rm -f ~/.bash_profile_flw
-ln -s ~/.flw-shell-profiles/dot_bash_profile_flw ~/.bash_profile_flw
+REPO_PATH=~/.flw-shell-profiles
+REPO_NAME=$(basename $REPO_PATH)
 
-sed -i '/# flw bash script begin/,/# flw bash script end/d' ~/.bash_profile
+install() {
+    dotname=$(basename $1)
+    realname=${dotname/#dot_/.}
+    if [ -z "$realname" ]; then
+        echo Cannot install $dotname.
+        return
+    fi
+    echo -n Install ~/$realname...
+    rm -f ~/$realname
+    ln -s $REPO_NAME/$dotname ~/$realname
+    echo done
+}
+
+# dot_config 的方案还有问题：
+# TODO: macOS 和 FreeBSD 上和其它软件的配置方法冲突怎么办，特别是 XDE 中
+# install dot_config                # 尽量通过 ~/.config/ 目录来配置，这样更集中
+
+install dot_bash_profile_flw
+install dot_bashrc_flw
+install dot_zshrc
+install dot_screenrc                # screen 配置文件
+install dot_tmux.conf               # tmux 和 tmate 的配置文件
+install dot_tmux-copy-mode.conf     # tmux 复制模式的配置文件，tmate 不支持所以另放
+install dot_fzf.zsh                 # fzf 配置文件
+install dot_inputrc                 # readline 配置文件
+install dot_gdbinit                 # gdb 启动脚本
+install dot_guile                   # guile 启动脚本
+install dot_sbclrc                  # sbcl 启动脚本
+install dot_haskeline               # Haskell REPL 配置，另外还要注意在 tmux 下配合修改
+install dot_racketrc                # Racket 启动配置
+install dot_editrc                  # Racket REPL 配置，另外还需要在 racketrc 里加载 xrepl
+install dot_gitconfig               # Git 配置文件
+
+# 安装 bash 启动脚本 {{{
+touch ~/.bash_profile
+perl -i -lne 'print unless /# flw bash script begin/../# flw bash script end/' ~/.bash_profile
 cat <<EOD >> ~/.bash_profile
 # flw bash script begin
 if [ -e ~/.bash_profile_flw ]; then
@@ -14,10 +48,8 @@ fi
 # flw bash script end
 EOD
 
-rm -f ~/.bashrc_flw
-ln -s ~/.flw-shell-profiles/dot_bashrc_flw ~/.bashrc_flw
-
-sed -i '/# flw bash script begin/,/# flw bash script end/d' ~/.bashrc
+touch ~/.bashrc
+perl -i -lne 'print unless /# flw bash script begin/../# flw bash script end/' ~/.bashrc
 cat <<EOD >> ~/.bashrc
 # flw bash script begin
 if [ -e ~/.bashrc_flw ]; then
@@ -28,63 +60,11 @@ EOD
 # }}}
 
 # 安装 oh-my-zsh 以及 zsh 启动脚本 {{{
-rm -f ~/.zshrc
-ln -s ~/.flw-shell-profiles/dot_zshrc ~/.zshrc
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-ln -s ~/.flw-shell-profiles/flw.zsh-theme ~/.oh-my-zsh/custom/themes/flw.zsh-theme
-# }}}
-
-# 安装 screen 配置文件 {{{
-rm -f ~/.screenrc
-ln -s ~/.flw-shell-profiles/dot_screenrc ~/.screenrc
-# }}}
-
-# 安装 tmux 配置文件 {{{
-rm -f ~/.tmux.conf
-ln -s ~/.flw-shell-profiles/dot_tmux.conf ~/.tmux.conf
-rm -f ~/.tmux-copy-mode.conf
-ln -s ~/.flw-shell-profiles/dot_tmux-copy-mode.conf ~/.tmux-copy-mode.conf
-# }}}
-
-# 安装 fzf 配置文件 {{{
-rm -f ~/.fzf.zsh
-ln -s ~/.flw-shell-profiles/dot_fzf.zsh ~/.fzf.zsh
+ln -s $REPO_PATH/flw.zsh-theme ~/.oh-my-zsh/custom/themes/flw.zsh-theme
 # }}}
 
 # 安装 docker 配置文件 {{{
-mkdir -p ~/.docker
-rm -f ~/.docker/config.json
-ln -s ~/.flw-shell-profiles/docker-config.json ~/.docker/config.json
-# }}}
-
-# 安装 readline 配置文件 {{{
-rm -f ~/.inputrc
-ln -s ~/.flw-shell-profiles/dot_inputrc ~/.inputrc
-# }}}
-
-# Haskell 不使用 readline 而是用 haskeline，另外还要注意在 tmux 下配合修改 {{{
-rm -f ~/.haskeline
-ln -s ~/.flw-shell-profiles/dot_haskeline ~/.haskeline
-# }}}
-
-# Racket 不使用 readline 而是用 libedit，另外还需要在启动时加载 xrepl {{{
-rm -f ~/.editrc
-ln -s ~/.flw-shell-profiles/dot_editrc ~/.editrc
-rm -f ~/.racketrc
-ln -s ~/.flw-shell-profiles/dot_racketrc ~/.racketrc
-# }}}
-
-# 安装 gdb 启动脚本 {{{
-rm -f ~/.gdbinit
-ln -s ~/.flw-shell-profiles/dot_gdbinit ~/.gdbinit
-# }}}
-
-# 安装 guile 启动脚本 {{{
-rm -f ~/.guile
-ln -s ~/.flw-shell-profiles/dot_guile ~/.guile
-# }}}
-
-# 安装 sbclrc 启动脚本 {{{
-rm -f ~/.sbclrc
-ln -s ~/.flw-shell-profiles/dot_sbclrc ~/.sbclrc
+rm -f ~/.docker
+ln -s ~/.config/docker ~/.docker
 # }}}
